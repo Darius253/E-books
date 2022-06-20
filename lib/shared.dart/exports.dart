96123d7b21ed457ece89d15e8b528ab774480d61
Darius <68332672+Darius253@users.noreply.github.com>
2022-06-20 01:38:21 +0000

    🆕 Added New files

---
+++ lib/shared.dart/exports.dart
@@ -0,0 +1,15 @@
+
+//pub
+export 'package:flutter_svg/flutter_svg.dart';
+
+
+
+
+
+//Local
+
+export 'package:reader_app/app.dart';
+export 'package:reader_app/onboarding/onboarding.dart';
+export 'package:reader_app/onboarding/sign_in.dart';
+export 'package:reader_app/onboarding/sign_up.dart';
+export 'package:reader_app/widgets/onboarding_button.dart';
