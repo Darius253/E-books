5731dd268b0afa21964510d3721ea9e91fb8e560
Darius <68332672+Darius253@users.noreply.github.com>
2022-06-22 00:57:43 +0000

    ⚙ Modified code

--- lib/onboarding/sign_in.dart
+++ lib/onboarding/sign_in.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../shared.dart/exports.dart';
+
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
@@ -10,6 +12,31 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(113, 144, 0, 255),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(40),
+              child: SvgPicture.asset(
+                'assets/images/sign_in.svg',
+                height: MediaQuery.of(context).size.height / 2,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 350.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(40),
+                child: Container(
+                  color: Colors.white,
+                  height: MediaQuery.of(context).size.height / 1.5,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
