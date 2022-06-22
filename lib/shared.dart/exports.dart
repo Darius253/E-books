96f50dd8574392fba185d9b1a11d189bac96a281
Darius <68332672+Darius253@users.noreply.github.com>
2022-06-22 00:58:07 +0000

    🆕 Added new widgets

--- lib/shared.dart/exports.dart
+++ lib/shared.dart/exports.dart
@@ -1,10 +1,6 @@
-
 //pub
 export 'package:flutter_svg/flutter_svg.dart';
-
-
-
-
+export 'package:get/get.dart';
 
 //Local
 
@@ -13,3 +9,7 @@ export 'package:reader_app/onboarding/onboarding.dart';
 export 'package:reader_app/onboarding/sign_in.dart';
 export 'package:reader_app/onboarding/sign_up.dart';
 export 'package:reader_app/widgets/onboarding_button.dart';
+export 'package:reader_app/shared.dart/constants.dart';
+export 'package:reader_app/onboarding/sign_up_creator.dart';
+export 'package:reader_app/onboarding/sign_up_subscriber.dart';
+export 'package:reader_app/onboarding/widgets/button.dart';
