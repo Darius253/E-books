b33cbf63b770f97eea1e0a22420ae73f346f79bb
Darius <68332672+Darius253@users.noreply.github.com>
2022-06-23 02:04:27 +0000

    ✨ Made few changes

--- lib/onboarding/sign_in.dart
+++ lib/onboarding/sign_in.dart
@@ -10,13 +10,22 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final TextEditingController _controller = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  String username = '';
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(113, 144, 0, 255),
+      backgroundColor: const Color.fromARGB(242, 237, 112, 23),
       body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
         child: Stack(
           children: [
+            const Back(),
             ClipRRect(
               borderRadius: BorderRadius.circular(40),
               child: SvgPicture.asset(
@@ -30,10 +39,102 @@ class _SignInState extends State<SignIn> {
                 borderRadius: BorderRadius.circular(40),
                 child: Container(
                   color: Colors.white,
-                  height: MediaQuery.of(context).size.height / 1.5,
+                  height: MediaQuery.of(context).size.height,
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 380.0),
+              child: Container(
+                color: Colors.white,
+                height: MediaQuery.of(context).size.height,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 50.0, horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Welcome Back',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 238, 140, 19),
+                            fontSize: 30),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Form(
+                          child: Column(
+                        children: [
+                          // Email Field
+                          TextFormField(
+                            controller: _controller,
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) => value!.isEmpty
+                                ? 'Please Enter a valid email or Username'
+                                : null,
+                            onChanged: (value) {
+                              setState(() => email = value);
+                            },
+                            decoration: const InputDecoration(
+                                hintText: 'Email or Username',
+                                hintStyle: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w400)),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+
+                          // Password Field
+                          TextFormField(
+                            obscureText: true,
+                            validator: (value) => value?.length != null
+                                ? 'Provide Password'
+                                : null,
+                            onChanged: (value) {
+                              setState(() => password = value);
+                            },
+                            controller: _passwordController,
+                            decoration: const InputDecoration(
+                                hintText: 'Password',
+                                hintStyle: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.w400)),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: const [
+                              Expanded(
+                                child: SizedBox(),
+                              ),
+                              Text(
+                                'Forgotten Password?',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 238, 140, 19),
+                                    fontWeight: FontWeight.w700),
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 60,
+                          ),
+                          Button(
+                            onPressed: () {},
+                            text: 'Sign In',
+                            word: "Don't have an Account? Sign In",
+                            onTap: () {
+                              Get.to(() => const SignUp());
+                            },
+                          ),
+                        ],
+                      )),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
