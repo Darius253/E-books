b644e2ac701205dac4ac4896a5c4bbb8e8d0ceb6
Darius <68332672+Darius253@users.noreply.github.com>
2022-06-27 01:17:59 +0000

    ⚙ Worked on validator

--- lib/onboarding/sign_up_subscriber.dart
+++ lib/onboarding/sign_up_subscriber.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 import '../shared.dart/exports.dart';
@@ -18,6 +20,8 @@ class _SubscriberSignUpState extends State<SubscriberSignUp> {
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
 
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   String username = '';
   String fullname = '';
   String email = '';
@@ -25,146 +29,279 @@ class _SubscriberSignUpState extends State<SubscriberSignUp> {
   String confirmpassword = '';
   String phonenumber = '';
 
+  String account_type = 'creator';
+  bool selected = false;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Username Field
-        TextFormField(
-          controller: _usernameController,
-          keyboardType: TextInputType.name,
-          validator: (value) =>
-              value!.length < 5 ? 'Username should be 5 to 10 characters' : '',
-          onChanged: (value) {
-            setState(() => username = value);
-          },
-          decoration: InputDecoration(
-              hintText: 'Username',
-              hintStyle: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                fontFamily: Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
-              )),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-
-        // Fullname field
-        TextFormField(
-          controller: _fullnameController,
-          keyboardType: TextInputType.name,
-          validator: (value) => value!.isEmpty
-              ? 'Kindly Provide your Fullname or Pseudonym'
-              : null,
-          onChanged: (value) {
-            setState(() => fullname = value);
-          },
-          decoration: InputDecoration(
-              hintText: 'Fullname/Pseudonym',
-              hintStyle: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                fontFamily: Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
-              )),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
-
-        //PhoneNumber
-        InternationalPhoneNumberInput(
-          onInputChanged: (PhoneNumber number) {
-            // print(number.phoneNumber);
-          },
-          onInputValidated: (bool value) {
-            // print(value);
-          },
-          selectorConfig: const SelectorConfig(
-            selectorType: PhoneInputSelectorType.DROPDOWN,
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          // Username Field
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _usernameController,
+            keyboardType: TextInputType.name,
+            validator: (value) => value!.length < 5
+                ? 'Username should be 5 to 10 characters'
+                : '',
+            onChanged: (value) {
+              setState(() => username = value);
+            },
+            decoration: InputDecoration(
+                hintText: 'Username',
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  fontFamily:
+                      Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
+                )),
           ),
-          ignoreBlank: false,
-          hintText: 'Phone Number',
-          selectorTextStyle: const TextStyle(color: Colors.black),
-          textFieldController: phoneNumberController,
-          formatInput: false,
-          validator: (value) => value == null ? 'Enter Phone Number' : '',
-          onFieldSubmitted: (value) {
-            setState(() => phonenumber = value);
-          },
-          keyboardType: const TextInputType.numberWithOptions(
-              signed: true, decimal: true),
-          onSaved: (PhoneNumber number) {
-            setState(() => phonenumber = number as String);
-          },
+          const SizedBox(
+            height: 15,
+          ),
+
+          // Fullname field
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _fullnameController,
+            keyboardType: TextInputType.name,
+            validator: (value) => value!.isEmpty
+                ? 'Kindly Provide your Fullname or Pseudonym'
+                : null,
+            onChanged: (value) {
+              setState(() => fullname = value);
+            },
+            decoration: InputDecoration(
+                hintText: 'Fullname/Pseudonym',
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  fontFamily:
+                      Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
+                )),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+
+          //PhoneNumber
+          InternationalPhoneNumberInput(
+            onInputChanged: (PhoneNumber number) {
+              // print(number.phoneNumber);
+            },
+            onInputValidated: (bool value) {
+              // print(value);
+            },
+            selectorConfig: const SelectorConfig(
+              selectorType: PhoneInputSelectorType.DROPDOWN,
+            ),
+            ignoreBlank: false,
+            hintText: 'Phone Number',
+            selectorTextStyle: const TextStyle(color: Colors.black),
+            textFieldController: phoneNumberController,
+            formatInput: false,
+            validator: (value) => value == null ? 'Enter Phone Number' : '',
+            onFieldSubmitted: (value) {
+              setState(() => phonenumber = value);
+            },
+            keyboardType: const TextInputType.numberWithOptions(
+                signed: true, decimal: true),
+            onSaved: (PhoneNumber number) {
+              setState(() => phonenumber = number as String);
+            },
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+
+          // Email Field
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            validator: (value) =>
+                value!.isEmail ? '' : 'Provide a valid Email address',
+            onChanged: (value) {
+              setState(() => email = value);
+            },
+            decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  fontFamily:
+                      Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
+                )),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+
+          // Password Field
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            obscureText: true,
+            validator: (value) => value!.length < 6
+                ? 'Password should be more than 5 characters'
+                : '',
+            onChanged: (value) {
+              setState(() => password = value);
+            },
+            controller: _passwordController,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  fontFamily:
+                      Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
+                )),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+
+          // Confirm Password Field
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            obscureText: true,
+            controller: _confirmPasswordController,
+            keyboardType: TextInputType.visiblePassword,
+            validator: (value) => value != password
+                ? ' Characters should match Password characters'
+                : '',
+            onChanged: (value) {
+              setState(() => confirmpassword = value);
+            },
+            decoration: InputDecoration(
+                hintText: 'Confirm Password',
+                hintStyle: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w400,
+                  fontFamily:
+                      Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
+                )),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          termsAndConditions(),
+          const SizedBox(
+            height: 60,
+          ),
+          Button(
+            onPressed: () => _showMyDialog(),
+            text: 'Sign Up',
+            word: 'Already Have an Account? Sign In',
+            onTap: () {
+              Get.to(() => const SignIn());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget termsAndConditions() {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Row(children: [
+        Container(
+          width: 18,
+          height: 18,
+          decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: Colors.black),
+              color: selected
+                  ? const Color.fromARGB(255, 237, 145, 33)
+                  : Colors.white),
         ),
         const SizedBox(
-          height: 15,
+          width: 3,
         ),
+        const Text('I agree to the Terms of Services and Privacy Policy'),
+      ]),
+    );
+  }
 
-        // Email Field
-        TextFormField(
-          controller: _emailController,
-          keyboardType: TextInputType.emailAddress,
-          validator: (value) =>
-              value!.isEmpty ? 'Please Enter a valid email' : '',
-          onChanged: (value) {
-            setState(() => email = value);
-          },
-          decoration: InputDecoration(
-              hintText: 'Email',
-              hintStyle: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                fontFamily: Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
-              )),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
+  Future<void> _showMyDialog() async => showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: account_type == 'creator'
+                ? const Text('Sign Up as a Creator')
+                : const Text('Sign Up as a Subscriber'),
+            content: SingleChildScrollView(
+              child: account_type == 'creator'
+                  ? ListBody(
+                      children: const <Widget>[
+                        Text('A Creator account allows you to: '),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                            '1. Upload your works on AwStore\n2. Purchase contents on AwStore\n3. Read and Download(optonal) contents on AwStore.'),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                            'Do you wish to proceed with creating an account type via this profile?'),
+                      ],
+                    )
+                  : ListBody(
+                      children: const <Widget>[
+                        Text('A Subscriber account allows you to: '),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                            '1. Purchase contents on AwStore.\n2. Read and Download(optional) contents on AwStore.'),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                            'Do you wish to proceed with creating an account type via this profile?'),
+                      ],
+                    ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                  child: const Text(
+                    'Proceed',
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+                  ),
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState?.save();
 
-        // Password Field
-        TextFormField(
-          obscureText: true,
-          validator: (value) => value!.length > 6
-              ? 'Password should be more than 5 characters'
-              : '',
-          onChanged: (value) {
-            setState(() => password = value);
-          },
-          controller: _passwordController,
-          decoration: InputDecoration(
-              hintText: 'Password',
-              hintStyle: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                fontFamily: Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
-              )),
-        ),
-        const SizedBox(
-          height: 15,
-        ),
+                      // loading();
+                    }
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        },
+      );
 
-        // Confirm Password Field
-        TextFormField(
-          obscureText: true,
-          controller: _confirmPasswordController,
-          keyboardType: TextInputType.visiblePassword,
-          validator: (value) => value != password
-              ? ' Characters should match Password characters'
-              : '',
-          onChanged: (value) {
-            setState(() => confirmpassword = value);
-          },
-          decoration: InputDecoration(
-              hintText: 'Confirm Password',
-              hintStyle: TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.w400,
-                fontFamily: Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
-              )),
-        ),
-      ],
+  Future<void> loading() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true, // user must tap button!
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          title: Text('Please Wait...'),
+          content: SizedBox(
+              width: 150,
+              height: 100,
+              child: Center(child: CircularProgressIndicator.adaptive())),
+        );
+      },
     );
   }
 }
