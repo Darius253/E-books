bc2fa5eea2c0e68ee386bbacc8a8b2c888842b65
Othniel Ussher <othnielussher16@gmail.com>
2022-07-05 16:11:53 +0000

    Add store tab

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,29 @@
-import 'package:flutter/cupertino.dart';
-import 'package:reader_app/onboarding/onboarding.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:reader_app/shared/exports.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
-    return const OnBoardingScreen();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return const GetCupertinoApp(
+      title: 'AwStore',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: <LocalizationsDelegate>[
+        DefaultMaterialLocalizations.delegate,
+        DefaultWidgetsLocalizations.delegate,
+      ],
+      // theme: ThemeData(
+      //   fontFamily: Platform.isIOS ? Font.sanfrancisco : Font.proxinova,
+      //   primaryColor: const Color.fromARGB(242, 237, 112, 23),
+      //   primarySwatch: Colors.orange,
+      // ),
+      // home: const OnBoardingScreen(),
+      home: NavBar(),
+    );
   }
 }
