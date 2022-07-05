77bf96dd75a1d623275bb0d05b7ac5337aeb50c7
Othniel Ussher <othnielussher16@gmail.com>
2022-07-05 16:12:41 +0000

    Add store tab

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,5 +1,35 @@
 import 'package:flutter/material.dart';
 
-class Palette {
-  static const Color primary = Color(0xFF8F00FF);
+class Doubles {
+  static const double headerMargin = 10.0;
+  static const double bodyMargin = 30.0;
 }
+
+class Palette {
+  static const Color primary = Color(0xFFED7117);
+  static const Color black = Color(0xFF200E32);
+  static Color black40 = const Color(0xFF200E32).withOpacity(.4);
+  static Color black80 = const Color(0xFF200E32).withOpacity(.8);
+}
+
+class Font {
+  static const String sanfrancisco = 'San Francisco';
+  static const String proxinova = 'Proxi Nova';
+}
+
+class Images {
+  static const String imagesPath = 'assets/images/';
+  static const String search = '${imagesPath}search.svg';
+  static const String searchO = '${imagesPath}search_o.svg';
+  static const String profile = '${imagesPath}profile.svg';
+  static const String profileO = '${imagesPath}profile_o.svg';
+  static const String library = '${imagesPath}library.svg';
+  static const String libraryO = '${imagesPath}library_o.svg';
+  static const String store = '${imagesPath}store.svg';
+  static const String storeO = '${imagesPath}store_o.svg';
+  static const String cart = '${imagesPath}cart.svg';
+  static const String cartO = '${imagesPath}cart_o.svg';
+}
+
+const lorem =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
