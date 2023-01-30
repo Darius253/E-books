ab6ebef4271cd20436fb8cab3f3d448762645f17
Darius <68332672+Darius253@users.noreply.github.com>
2023-01-30 22:57:16 +0000

    ✔⚙ Worked on Sign in pages

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -4,7 +4,8 @@ export 'dart:io' show Platform;
 // pub
 export 'package:get/get.dart';
 export 'package:flutter_svg/flutter_svg.dart';
-
+export 'package:pin_code_fields/pin_code_fields.dart';
+export 'package:flutter_password_strength/flutter_password_strength.dart';
 // controllers
 
 // models
@@ -15,4 +16,6 @@ export 'package:reader_app/shared/constants.dart';
 export 'package:reader_app/onboarding/sign_in.dart';
 export 'package:reader_app/widgets/back_button.dart';
 export 'package:reader_app/onboarding/forgot_password.dart';
+export 'package:reader_app/onboarding/widgets/button.dart';
+export 'package:reader_app/onboarding/verify_email.dart';
 
