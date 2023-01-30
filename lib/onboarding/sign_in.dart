ecb3403ed86c44b04990ceb6ad22b2269a4bff5d
Darius <68332672+Darius253@users.noreply.github.com>
2023-01-30 01:11:39 +0000

    ⚙Worked on sign in

--- lib/onboarding/sign_in.dart
+++ lib/onboarding/sign_in.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:reader_app/shared/exports.dart';
+import 'package:reader_app/onboarding/widgets/button.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -13,6 +13,11 @@ class _SignInState extends State<SignIn> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  bool obscurePassword = false;
+  Text terms = const Text(
+    'Terms and Conditions',
+    style: TextStyle(color: Color.fromARGB(216, 237, 112, 23)),
+  );
 
   String email = '';
   String password = '';
@@ -26,65 +31,168 @@ class _SignInState extends State<SignIn> {
             horizontal: MediaQuery.of(context).size.width * 0.05, vertical: 24),
         child: SingleChildScrollView(
           child: Column(children: [
-            const Center(
-              child: Text('LOGO'),
+            Center(
+              child: Column(
+                children: [
+                  const Text('LOGO'),
+                  const SizedBox(height: 30),
+                  const Text('Welcome back, Darius!',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Color.fromARGB(209, 5, 17, 36),
+                        fontSize: 18,
+                      )),
+                  const SizedBox(height: 5),
+                  const Text(
+                      'Enter your credentials to sign in to your account',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromARGB(214, 165, 165, 165))),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+                ],
+              ),
             ),
-            const SizedBox(height: 15),
-            const Text('Welcome back, Darius!',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Color.fromARGB(209, 5, 17, 36),
-                  fontSize: 18,
-                )),
-            const SizedBox(height: 5),
-            const Text('Enter your credentials to sign in to your account',
-                style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Color.fromARGB(214, 165, 165, 165))),
-            const SizedBox(height: 20),
             Form(
-                key: formKey,
-                
-                child:  Column(
-                  children: [
-                    //Email TextField
-                    TextFormField(
-                      decoration: InputDecoration(
-                          suffixIcon:
-                              const Icon(CupertinoIcons.mail, color: Colors.grey),
-                          labelText: 'Email',
-                          labelStyle: const TextStyle(
-                            color: Color.fromARGB(214, 165, 165, 165),
-                            fontWeight: FontWeight.w400,
-                          ),
-                          focusColor: const Color.fromARGB(216, 237, 112, 23),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(216, 237, 112, 23)),
-                              borderRadius: BorderRadius.circular(8)),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8))),
-                    ),
-                    //Password Field
-                    TextFormField(
-                      decoration: InputDecoration(
-                          suffixIcon:
-                              const Icon(CupertinoIcons.eye, color: Colors.grey),
-                          labelText: 'Enter Password',
-                          labelStyle: const TextStyle(
-                            color: Color.fromARGB(214, 165, 165, 165),
-                            fontWeight: FontWeight.w400,
-                          ),
-                          focusColor: const Color.fromARGB(216, 237, 112, 23),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                  color: Color.fromARGB(216, 237, 112, 23)),
-                              borderRadius: BorderRadius.circular(8)),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8))),
-                    ),
-                  ],
-                ),),
+              key: formKey,
+              child: Column(
+                children: [
+                  //Email TextField
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        suffixIcon:
+                            const Icon(CupertinoIcons.mail, color: Colors.grey),
+                        labelText: 'Email',
+                        labelStyle: const TextStyle(
+                          color: Color.fromARGB(214, 165, 165, 165),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Color.fromARGB(216, 237, 112, 23)),
+                            borderRadius: BorderRadius.circular(8)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8))),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  //Password Field
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: !obscurePassword,
+                    decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: obscurePassword == true
+                              ? const Icon(CupertinoIcons.eye,
+                                  color: Colors.grey)
+                              : const Icon(CupertinoIcons.eye_slash,
+                                  color: Colors.grey),
+                          onPressed: () {
+                            setState(() {
+                              obscurePassword = !obscurePassword;
+                            });
+                          },
+                        ),
+                        labelText: 'Enter Password',
+                        labelStyle: const TextStyle(
+                          color: Color.fromARGB(214, 165, 165, 165),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
+                        ),
+                        focusColor: const Color.fromARGB(216, 237, 112, 23),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Color.fromARGB(216, 237, 112, 23)),
+                            borderRadius: BorderRadius.circular(8)),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8))),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                const Expanded(child: SizedBox()),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Forgot Password?',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromARGB(190, 237, 112, 23))),
+                )
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Button(text: 'Login', onTap: () {}),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: 2,
+                  color: const Color.fromARGB(255, 131, 130, 130),
+                ),
+                const Text(
+                  'Or Continue with',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  height: 2,
+                  color: const Color.fromARGB(255, 131, 130, 130),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.032,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.facebook,
+                      color: Color.fromARGB(255, 11, 132, 238),
+                      size: 30,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      CupertinoIcons.arrow_counterclockwise_circle,
+                      color: Color.fromARGB(255, 11, 132, 238),
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.065,
+            ),
+            const Text.rich(
+              TextSpan(
+                  text: 'By logging in, I have read and agreed to the ',
+                  style: TextStyle(fontSize: 12),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Terms and Condition ',
+                        style: TextStyle(
+                            color: Color.fromARGB(190, 237, 112, 23),
+                            fontWeight: FontWeight.w700,
+                            fontSize: 12)),
+                    TextSpan(
+                        text: 'Use Policy and Privacy Policy',
+                        style: TextStyle(fontSize: 12))
+                  ]),
+              textAlign: TextAlign.center,
+            )
           ]),
         ),
       )),
