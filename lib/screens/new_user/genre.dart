393a053fd9947c09d8040eefe79887993e267655
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-06 23:14:32 +0000

    🔨🆕 Made changes

---
+++ lib/screens/new_user/genre.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:reader_app/widgets/genre_card.dart';
+
+class SlectGenre extends StatefulWidget {
+  const SlectGenre({super.key});
+
+  @override
+  State<SlectGenre> createState() => _SlectGenreState();
+}
+
+class _SlectGenreState extends State<SlectGenre> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
+    List genres = [
+      genreCard(width, height, 'Horro', 'horro.svg'),
+      genreCard(width, height, 'Adventure', 'adventure.svg'),
+      genreCard(width, height, 'Hisstory', 'history.svg'),
+      genreCard(width, height, 'Mystery', 'mystery.svg'),
+      genreCard(width, height, 'Science', 'science.svg'),
+      genreCard(width, height, 'Romance', 'romance.svg'),
+      genreCard(width, height, 'Comedy', 'comedy.svg'),
+      genreCard(width, height, 'Fiction', 'fiction.svg'),
+      genreCard(width, height, 'Poetry', 'poetry.svg'),
+      genreCard(width, height, 'Arts', 'arts.svg'),
+      genreCard(width, height, 'Crime', 'crime.svg'),
+      genreCard(width, height, 'Sports', 'sports.svg'),
+    ];
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05,
+                  vertical: MediaQuery.of(context).size.height * 0.03),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Column(
+                        children: [
+                          const Text('Hello, Darius!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromARGB(235, 237, 112, 23),
+                                fontSize: 16,
+                              )),
+                          const SizedBox(height: 8),
+                          const Text(
+                              'Please select 3 genres that you like to read. This will help us recommend books  based on your interests',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromARGB(251, 0, 0, 0))),
+                          SizedBox(
+                              height:
+                                  MediaQuery.of(context).size.height * 0.032),
+                        ],
+                      ),
+                    ),
+                    genreCard(width, height, 'Horro', 'horro.svg'),
+                    //         GridView.count(
+                    //     crossAxisCount: 3,
+                    //     crossAxisSpacing: 4.0,
+                    //     mainAxisSpacing: 8.0,
+                    //     children: List.generate(genres.length, (index) {
+                    //       return Center(
+                    //         child: genreCard(width, height),
+                    //       );
+                    //     }
+                    //     )
+                    // )
+                  ],
+                ),
+              ))),
+    );
+  }
+}
---
+++ lib/screens/new_user/signup/sign_up_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import '../../../shared/exports.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  String accountType = 'customer';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Align(
+              alignment: Alignment.center,
+              child: Image.asset('assets/images/welcome.png'),
+            ),
+          ),
+          const SizedBox(height: 16),
+          creatorORcustomerButtons(),
+          // const SizedBox(
+          //   height: 16,
+          // ),
+          Expanded(child: SingleChildScrollView(child: creatorORcustomer())),
+        ],
+      ),
+    );
+  }
+
+  Widget creatorORcustomer() {
+    if (accountType == 'customer') {
+      return const CustomerSignUpPage();
+    } else {
+      return const CreatorSignUpPage();
+    }
+  }
+
+  Widget creatorORcustomerButtons() {
+    return SizedBox(
+      height: 45,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButtons(
+            btext: 'Seller',
+            // bbackgroundColor:
+            //     accountType == 'creator' ? Palette.primary : Colors.transparent,
+            bcolor:
+                accountType == 'creator' ? Palette.primary : Colors.transparent,
+            onTap: () {
+              setState(() {
+                accountType = 'creator';
+              });
+            },
+            textcolor: accountType == 'creator' ? Palette.primary : Colors.grey,
+          ),
+          Container(
+            width: 1,
+            height: 26,
+            color: Palette.grey,
+          ),
+          TextButtons(
+            btext: 'Customer',
+            //bbackgroundColor: accountType == 'customer' ? Palette.primary : Colors.transparent,
+            bcolor: accountType == 'customer'
+                ? Palette.primary
+                : Colors.transparent,
+            onTap: () {
+              setState(() {
+                accountType = 'customer';
+              });
+            },
+            textcolor: accountType == 'creator' ? Colors.grey : Palette.primary,
+          ),
+        ],
+      ),
+    );
+  }
+}
