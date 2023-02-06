f8d017475bb727063c0a72c47a53dfa69edb027a
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-06 21:19:55 +0000

    Made changes

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -6,6 +6,8 @@ export 'package:get/get.dart';
 export 'package:flutter_svg/flutter_svg.dart';
 export 'package:pin_code_fields/pin_code_fields.dart';
 export 'package:flutter_password_strength/flutter_password_strength.dart';
+export 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
 // controllers
 
 // models
@@ -18,10 +20,6 @@ export 'package:reader_app/widgets/back_button.dart';
 export 'package:reader_app/onboarding/forgot_password.dart';
 export 'package:reader_app/onboarding/widgets/button.dart';
 export 'package:reader_app/onboarding/verify_email.dart';
-export 'package:reader_app/onboarding/sign_up_subscriber.dart';
-export 'package:reader_app/screens/store/widgets/book_info_item.dart';
-export 'package:reader_app/screens/store/widgets/book_store_item.dart';
-export 'package:reader_app/screens/store/widgets/store_book_detail.dart';
 export 'package:reader_app/widgets/creater_button.dart';
 export 'package:reader_app/screens/signup/select_sign_up.dart';
 export  'package:reader_app/screens/signup/sign_up_page.dart';
