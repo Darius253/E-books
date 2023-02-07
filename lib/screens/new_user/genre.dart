df16740a0d0ab5e00f010a32290a3ecdae301c83
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-07 19:48:30 +0000

    🆕✨ Worked on genre page

--- lib/screens/new_user/genre.dart
+++ lib/screens/new_user/genre.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:reader_app/widgets/genre_card.dart';
 
+import '../../shared/exports.dart';
+
 class SlectGenre extends StatefulWidget {
   const SlectGenre({super.key});
 
@@ -14,67 +16,69 @@ class _SlectGenreState extends State<SlectGenre> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    List genres = <ListTile>[
-      
-      // genreCard(width, height, 'Horro', 'horro.svg'),
-      // genreCard(width, height, 'Adventure', 'adventure.svg'),
-      // genreCard(width, height, 'Hisstory', 'history.svg'),
-      // genreCard(width, height, 'Mystery', 'mystery.svg'),
-      // genreCard(width, height, 'Science', 'science.svg'),
-      // genreCard(width, height, 'Romance', 'romance.svg'),
-      // genreCard(width, height, 'Comedy', 'comedy.svg'),
-      // genreCard(width, height, 'Fiction', 'fiction.svg'),
-      // genreCard(width, height, 'Poetry', 'poetry.svg'),
-      // genreCard(width, height, 'Arts', 'arts.svg'),
-      // genreCard(width, height, 'Crime', 'crime.svg'),
-      // genreCard(width, height, 'Sports', 'sports.svg'),
-    ];
     return Scaffold(
       body: SafeArea(
           child: Padding(
               padding: EdgeInsets.symmetric(
                   horizontal: MediaQuery.of(context).size.width * 0.05,
                   vertical: MediaQuery.of(context).size.height * 0.03),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Column(
-                        children: [
-                          const Text('Hello, Darius!',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                color: Color.fromARGB(235, 237, 112, 23),
-                                fontSize: 16,
-                              )),
-                          const SizedBox(height: 8),
-                          const Text(
-                              'Please select 3 genres that you like to read. This will help us recommend books  based on your interests',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  color: Color.fromARGB(251, 0, 0, 0))),
-                          SizedBox(
-                              height:
-                                  MediaQuery.of(context).size.height * 0.032),
-                        ],
-                      ),
+              child: ListView(
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        const Text('Hello, Darius!',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Color.fromARGB(235, 237, 112, 23),
+                              fontSize: 16,
+                            )),
+                        const SizedBox(height: 8),
+                        const Text(
+                            'Please select 3 genres that you like to read. This will help us recommend books  based on your interests',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                color: Color.fromARGB(251, 0, 0, 0))),
+                        SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.032),
+                      ],
                     ),
-                    genreCard(width, height, 'Horro', 'romance.svg'),
-            //               GridView.count(  
-            //     crossAxisCount: 3,  
-            //     crossAxisSpacing: 4.0,  
-            //     mainAxisSpacing: 8.0,  
-            //     children: List.generate(genres.length, (index) {  
-            //       return genreCard(width, height, genre, image);  
-                    
-            //     }  
-            //     )  
-            // ) 
-                  ],
-                ),
+                  ),
+                  Column(children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GenreCard(
+                          genre: genres[0],
+                        ),
+                        const SizedBox(
+                          width: 17,
+                        ),
+                        GenreCard(
+                          genre: genres[1],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 25,
+                    ),
+                    Row(
+                      children: [
+                        GenreCard(
+                          genre: genres[2],
+                        ),
+                        const SizedBox(
+                          width: 17,
+                        ),
+                        GenreCard(
+                          genre: genres[3],
+                        ),
+                      ],
+                    ),
+                  ])
+                ],
               ))),
     );
   }
