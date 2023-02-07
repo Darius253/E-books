0be234c12a6827c60ccda746421fd899ab5e5d80
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-07 19:47:35 +0000

    🆕✨ Worked on genre page

--- lib/models/genre_model.dart
+++ lib/models/genre_model.dart
@@ -1,27 +1,46 @@
-import '../shared/constants.dart';
+import 'package:flutter/material.dart';
+
+import '../shared/exports.dart';
 
 class Genre {
   String text;
   String image;
+  CustomClipper<Path>? clipper;
 
-  Genre({
-    required this.text,
-    required this.image,
-  });
+  Genre({required this.text, required this.image, required this.clipper});
 }
 
-
- List<Genre> genres = [
-    Genre(image: Images.horror, text: 'Horro'),
-    Genre(image: Images.adventure, text: 'Adventure'),
-    Genre(image: Images.history, text: 'History'),
-    Genre(image: Images.mystery, text: 'Mystery'),
-    Genre(image: Images.science, text: 'Science'),
-    Genre(image: Images.romance, text: 'Romance'),
-    Genre(image: Images.comedy, text: 'Comedy'),
-    Genre(image: Images.fiction, text: 'Fiction'),
-    Genre(image: Images.poetry, text: 'Poetry'),
-    Genre(image: Images.arts, text: 'Arts'),
-    Genre(image: Images.crime, text: 'Crime'),
-    Genre(image: Images.sports, text: 'Sports'),
-  ];
+List<Genre> genres = [
+  Genre(image: Images.horror, text: 'Horror', clipper: HexagonalClipper()),
+  Genre(
+      image: Images.adventure,
+      text: 'Adventure',
+      clipper: OvalLeftBorderClipper()),
+  Genre(
+      image: Images.history, text: 'History', clipper: OvalTopBorderClipper()),
+  Genre(
+      image: Images.mystery,
+      text: 'Mystery',
+      clipper: WaveClipperOne(reverse: true, flip: true)),
+  Genre(
+      image: Images.science, text: 'Science', clipper: OvalTopBorderClipper()),
+  Genre(
+      image: Images.romance,
+      text: 'Romance',
+      clipper: MovieTicketBothSidesClipper()),
+  Genre(image: Images.comedy, text: 'Comedy', clipper: OvalLeftBorderClipper()),
+  Genre(
+      image: Images.fiction,
+      text: 'Fiction',
+      clipper: WaveClipperOne(reverse: true, flip: true)),
+  Genre(image: Images.poetry, text: 'Poetry', clipper: OvalTopBorderClipper()),
+  Genre(image: Images.arts, text: 'Arts', clipper: OctagonalClipper()),
+  Genre(
+      image: Images.crime,
+      text: 'Crime',
+      clipper: WaveClipperOne(flip: true, reverse: true)),
+  Genre(
+      image: Images.sports,
+      text: 'Sports',
+      clipper: HexagonalClipper(reverse: true)),
+];
