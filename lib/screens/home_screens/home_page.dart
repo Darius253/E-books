cdf24ad65b8923f5180b1abfbae8af40d3a94f0f
Darius <68332672+Darius253@users.noreply.github.com>
2023-02-08 16:28:38 +0000

    🆕🔨 Made changes

---
+++ lib/screens/home_screens/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width * 0.05,
+            vertical: MediaQuery.of(context).size.height * 0.03),
+        child: Stack(children: [
+          
+        ],),
+    )));
+  }
+}
