0e6f5821461d2d677d43234721b0afd4250a6593
Darius <68332672+Darius253@users.noreply.github.com>
2023-03-02 00:58:49 +0000

    🆕✨ Worked on Book Preview

--- lib/screens/home_screens/widgets/book_shelf.dart
+++ lib/screens/home_screens/widgets/book_shelf.dart
@@ -1,68 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:reader_app/screens/home_screens/book_preview.dart';
 
 class BookShelf extends StatelessWidget {
   const BookShelf({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<String> genre = ['Adventure', 'Romance', 'Horror', 'Sports'];
+    List<String> book = [
+      'My life',
+      'How I Met Your Mother',
+      'We Broke Up',
+      'Dream Big'
+    ];
+
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [
-              container(width, height, 'Currently Reading'),
-              SizedBox(width: width * 0.07),
-              container(width, height, 'New Releases'),
-              SizedBox(width: width * 0.07),
-            ],
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      SingleChildScrollView(
+        child: SizedBox(
+          width: width * 0.9,
+          height: height * 0.435,
+          child: ListView.separated(
+              shrinkWrap: true,
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  width: width * 0.07,
+                );
+              },
+              scrollDirection: Axis.horizontal,
+              itemCount: 3,
+              itemBuilder: (BuildContext context, int index) {
+                return container(width, height, 'New Release', context);
+              }),
+        ),
+      ),
+      SizedBox(
+        width: width * 0.9,
+        height: height * 0.4,
+        child: ListView.separated(
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: height * 0.05,
+              );
+            },
+            itemCount: genre.length,
+            itemBuilder: (BuildContext context, int index) {
+              return favouriteGenre(height, width, genre[index], book[index],
+                  'Darius Tron', 'GHS 89.99', context);
+            }),
+      )
+    ]);
+  }
+
+  Widget container(double width, height, String text, BuildContext context) {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            text,
+            style: const TextStyle(
+                wordSpacing: 3,
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 18),
           ),
-        ),
-        SizedBox(height: height * 0.07),
-        favouriteGenre(
-            height, width, 'Adventure', 'My Life', 'Darius Tron', 'GHS 89.99'),
-        SizedBox(height: height * 0.07),
-        favouriteGenre(height, width, 'Romance', 'How I Met Your Mother',
-            'Darius Tron', 'GHS 100'),
-        SizedBox(height: height * 0.07),
-        favouriteGenre(
-            height, width, 'Horror', 'We Broke Up', 'Darius Tron', 'GHS 150'),
-      ],
+          SizedBox(
+            height: height * 0.02,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(
+                horizontal: width * 0.07, vertical: height * 0.025),
+            width: width * 0.7,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                border:
+                    Border.all(color: const Color.fromARGB(255, 140, 63, 4))),
+            child: genreBook(
+                height, width, 'My Life', 'Darius', 'GHS 89.99', context),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget container(double width, height, String text) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          text,
-          style: const TextStyle(
-              wordSpacing: 3,
-              color: Colors.black,
-              fontWeight: FontWeight.w600,
-              fontSize: 18),
-        ),
-        SizedBox(
-          height: height * 0.025,
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: width*0.07, vertical: height*0.025),
-          width: width * 0.7,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(color: const Color.fromARGB(255, 140, 63, 4))),
-          child: genreBook(height, width, 'My Life', 'Darius', 'GHS 89.99'),
-        )
-      ],
-    );
-  }
-
-  Widget favouriteGenre(
-      double height, width, String genre, title, author, price) {
+  Widget favouriteGenre(double height, width, String genre, title, author,
+      price, BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -87,13 +111,13 @@ class BookShelf extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              genreBook(height, width, title, author, price),
+              genreBook(height, width, title, author, price, context),
               SizedBox(width: width * 0.07),
-              genreBook(height, width, title, author, price),
+              genreBook(height, width, title, author, price, context),
               SizedBox(width: width * 0.07),
-              genreBook(height, width, title, author, price),
+              genreBook(height, width, title, author, price, context),
               SizedBox(width: width * 0.07),
-              genreBook(height, width, title, author, price),
+              genreBook(height, width, title, author, price, context),
               SizedBox(width: width * 0.07),
             ],
           ),
@@ -102,45 +126,51 @@ class BookShelf extends StatelessWidget {
     );
   }
 
-  Widget genreBook(double height, width, title, author, price) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: width * 0.45,
-          height: height * 0.25,
-          decoration: BoxDecoration(
-              border: Border.all(color: const Color.fromARGB(255, 140, 63, 4)),
-              borderRadius: BorderRadius.circular(8)),
-        ),
-        SizedBox(
-          height: height * 0.01,
-        ),
-        Text(
-          title,
-          style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
-        ),
-        SizedBox(
-          height: height * 0.01,
-        ),
-        Text(
-          'By $author',
-          style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
-        ),
-        SizedBox(
-          height: height * 0.01,
-        ),
-        Text(
-          price,
-          style: const TextStyle(
-              fontWeight: FontWeight.w700,
-              fontSize: 16,
-              color: Color.fromARGB(255, 140, 63, 4)),
-        ),
-        SizedBox(
-          height: height * 0.01,
-        ),
-      ],
+  Widget genreBook(
+      double height, width, title, author, price, BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(builder: ((context) => const BookPreview()))),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: width * 0.4,
+            height: height * 0.2,
+            decoration: BoxDecoration(
+                border:
+                    Border.all(color: const Color.fromARGB(255, 140, 63, 4)),
+                borderRadius: BorderRadius.circular(8)),
+          ),
+          SizedBox(
+            height: height * 0.01,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
+          ),
+          SizedBox(
+            height: height * 0.01,
+          ),
+          Text(
+            'By $author',
+            style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+          ),
+          SizedBox(
+            height: height * 0.01,
+          ),
+          Text(
+            price,
+            style: const TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+                color: Color.fromARGB(255, 140, 63, 4)),
+          ),
+          SizedBox(
+            height: height * 0.01,
+          ),
+        ],
+      ),
     );
   }
 }
