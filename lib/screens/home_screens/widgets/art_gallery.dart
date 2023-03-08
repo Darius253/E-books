ac7cf442439cd2233b611617f6e917eeca5907b8
Darius <68332672+Darius253@users.noreply.github.com>
2023-03-08 01:35:47 +0000

    ✔✏ Worked on Art Gallery

---
+++ lib/screens/home_screens/widgets/art_gallery.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:reader_app/shared/exports.dart';
+
+class ArtGallery extends StatelessWidget {
+  const ArtGallery({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              artCategory(width, height, 'New Arts for You'),
+              SizedBox(
+                  height: height * 0.48,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 8,
+                      itemBuilder: (BuildContext context, int index) {
+                        return art(
+                            width * 0.65,
+                            height * 0.34,
+                            'Darius Tron',
+                            'Golden Palm',
+                            'Dreamland Gallery',
+                            '1,500',
+                            17);
+                      })),
+              SizedBox(
+                height: height * 0.04,
+              ),
+              artCategory(width, height, 'Auctions'),
+              SizedBox(
+                  height: height * 0.35,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 8,
+                      itemBuilder: (BuildContext context, int index) {
+                        return art(
+                            width * 0.55,
+                            height * 0.2,
+                            'Darius Tron',
+                            'Golden Palm',
+                            'Dreamland Gallery',
+                            'Jan 19 at 6:00pm GMT',
+                            13);
+                      })),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              artCategory(
+                width,
+                height,
+                'Weekly Trends',
+              ),
+              SizedBox(
+                  height: height * 0.35,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 8,
+                      itemBuilder: (BuildContext context, int index) {
+                        return art(
+                            width * 0.55,
+                            height * 0.2,
+                            'Darius Tron',
+                            'Golden Palm',
+                            'Dreamland Gallery',
+                            '1,500',
+                            17);
+                      })),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              artCategory(width, height, 'Popular Arts'),
+              SizedBox(
+                  height: height * 0.35,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 8,
+                      itemBuilder: (BuildContext context, int index) {
+                        return art(
+                            width * 0.55,
+                            height * 0.2,
+                            'Darius Tron',
+                            'Golden Palm',
+                            'Dreamland Gallery',
+                            '1,500',
+                            17);
+                      })),
+                       SizedBox(
+                height: height * 0.075,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget artCategory(double width, height, String category) {
+    return Padding(
+        padding: EdgeInsets.only(left: width * 0.07, bottom: height * 0.02),
+        child: Text(
+          category,
+          style: const TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 17,
+          ),
+        ));
+  }
+
+  
+
+}
