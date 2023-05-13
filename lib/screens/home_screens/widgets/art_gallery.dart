c7bcbe23ba0a3a4e1e578ce641e04ed599128bff
Darius <68332672+Darius253@users.noreply.github.com>
2023-05-13 21:10:30 +0000

    Made few modifications

--- lib/screens/home_screens/widgets/art_gallery.dart
+++ lib/screens/home_screens/widgets/art_gallery.dart
@@ -9,97 +9,89 @@ class ArtGallery extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return SafeArea(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 10),
+        child: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: AnimationLimiter(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              artCategory(width, height, 'New Arts for You'),
-              SizedBox(
-                  height: height * 0.48,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 8,
-                      itemBuilder: (BuildContext context, int index) {
-                        return art(
-                            width * 0.65,
-                            height * 0.34,
-                            'Darius Tron',
-                            'Golden Palm',
-                            'Dreamland Gallery',
-                            '1,500',
-                            17);
-                      })),
-              SizedBox(
-                height: height * 0.04,
+            children: AnimationConfiguration.toStaggeredList(
+              duration: const Duration(milliseconds: 400),
+              childAnimationBuilder: (widget) => SlideAnimation(
+                horizontalOffset: 100.0,
+                child: FlipAnimation(
+                  child: widget,
+                ),
               ),
-              artCategory(width, height, 'Auctions'),
-              SizedBox(
-                  height: height * 0.35,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 8,
-                      itemBuilder: (BuildContext context, int index) {
-                        return art(
-                            width * 0.55,
-                            height * 0.2,
-                            'Darius Tron',
-                            'Golden Palm',
-                            'Dreamland Gallery',
-                            'Jan 19 at 6:00pm GMT',
-                            13);
-                      })),
-              SizedBox(
-                height: height * 0.02,
-              ),
-              artCategory(
-                width,
-                height,
-                'Weekly Trends',
-              ),
-              SizedBox(
-                  height: height * 0.35,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 8,
-                      itemBuilder: (BuildContext context, int index) {
-                        return art(
-                            width * 0.55,
-                            height * 0.2,
-                            'Darius Tron',
-                            'Golden Palm',
-                            'Dreamland Gallery',
-                            '1,500',
-                            17);
-                      })),
-              SizedBox(
-                height: height * 0.02,
-              ),
-              artCategory(width, height, 'Popular Arts'),
-              SizedBox(
-                  height: height * 0.35,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 8,
-                      itemBuilder: (BuildContext context, int index) {
-                        return art(
-                            width * 0.55,
-                            height * 0.2,
-                            'Darius Tron',
-                            'Golden Palm',
-                            'Dreamland Gallery',
-                            '1,500',
-                            17);
-                      })),
-                       SizedBox(
-                height: height * 0.02,
-              ),
-            ],
+              children: [
+                artCategory(width, height, 'New Arts for You'),
+                SizedBox(
+                    height: height * 0.48,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 8,
+                        itemBuilder: (BuildContext context, int index) {
+                          return art(width * 0.65, height * 0.34, 'Darius Tron',
+                              'Golden Palm', 'Dreamland Gallery', '1,500', 17, context);
+                        })),
+                SizedBox(
+                  height: height * 0.04,
+                ),
+                artCategory(width, height, 'Auctions'),
+                SizedBox(
+                    height: height * 0.35,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 8,
+                        itemBuilder: (BuildContext context, int index) {
+                          return art(
+                              width * 0.55,
+                              height * 0.2,
+                              'Darius Tron',
+                              'Golden Palm',
+                              'Dreamland Gallery',
+                              'Jan 19 at 6:00pm GMT',
+                              13, context);
+                        })),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+                artCategory(
+                  width,
+                  height,
+                  'Weekly Trends',
+                ),
+                SizedBox(
+                    height: height * 0.35,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 8,
+                        itemBuilder: (BuildContext context, int index) {
+                          return art(width * 0.55, height * 0.2, 'Darius Tron',
+                              'Golden Palm', 'Dreamland Gallery', '1,500', 17, context);
+                        })),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+                artCategory(width, height, 'Popular Arts'),
+                SizedBox(
+                    height: height * 0.35,
+                    child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 8,
+                        itemBuilder: (BuildContext context, int index) {
+                          return art(width * 0.55, height * 0.2, 'Darius Tron',
+                              'Golden Palm', 'Dreamland Gallery', '1,500', 17, context);
+                        })),
+                SizedBox(
+                  height: height * 0.02,
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    );
+    ));
   }
 
   Widget artCategory(double width, height, String category) {
@@ -113,7 +105,4 @@ class ArtGallery extends StatelessWidget {
           ),
         ));
   }
-
-  
-
 }
