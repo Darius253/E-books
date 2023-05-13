3c108f040147e5689e0f9a7e037ad5d04e756a4d
Darius <68332672+Darius253@users.noreply.github.com>
2023-05-13 21:10:07 +0000

    Made few modifications

--- lib/screens/authenticate/new_user/signup/widgets/terms_and_condition.dart
+++ lib/screens/authenticate/new_user/signup/widgets/terms_and_condition.dart
@@ -1,32 +1,45 @@
 import 'package:flutter/material.dart';
 
 class TermsAndConditions extends StatelessWidget {
-  final String label;
   final bool value;
   final Function onChanged;
   const TermsAndConditions(
-      {super.key,
-      required this.label,
-      required this.value,
-      required this.onChanged});
+      {super.key, required this.value, required this.onChanged});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(
+        Checkbox.adaptive(
             activeColor: Colors.green,
             value: value,
             onChanged: (bool? newValue) {
               onChanged(newValue);
             }),
-        SizedBox(
-          width: 270,
-          child: Text(
-            label,
-            style: const TextStyle(
-              fontSize: 12,
-            ),
+        const Expanded(
+          child: Text.rich(
+            TextSpan(
+                text: 'I agree to the ',
+                style: TextStyle(fontSize: 12),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Terms and Condition ',
+                      style: TextStyle(
+                          color: Color.fromARGB(190, 237, 112, 23),
+                          fontWeight: FontWeight.w700,
+                          decoration: TextDecoration.underline,
+                          fontSize: 12)),
+                  TextSpan(
+                      text: 'of services and ', style: TextStyle(fontSize: 12)),
+                  TextSpan(
+                      text: 'Privacy Policy',
+                      style: TextStyle(
+                        fontSize: 12,
+                        decoration: TextDecoration.underline,
+                        color: Color.fromARGB(190, 237, 112, 23),
+                      ))
+                ]),
+            textAlign: TextAlign.left,
           ),
         ),
       ],
