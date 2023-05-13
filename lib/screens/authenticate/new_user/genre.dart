d29e7a4958d47c792c90745f71d90908b86c0764
Darius <68332672+Darius253@users.noreply.github.com>
2023-05-13 21:08:46 +0000

    Made few modifications

--- lib/screens/authenticate/new_user/genre.dart
+++ lib/screens/authenticate/new_user/genre.dart
@@ -15,42 +15,44 @@ class _SelectGenreState extends State<SelectGenre> {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.05,
-                  vertical: MediaQuery.of(context).size.height * 0.01),
-              child: ListView(
-                children: [
-                  Center(
-                    child: Column(
-                      children: [
-                        const Text('Hello, Darius!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              color: Color.fromARGB(235, 237, 112, 23),
-                              fontSize: 16,
-                            )),
-                        const SizedBox(height: 8),
-                        const Text(
-                            'Please select 3 genres that you like to read. This will help us recommend books  based on your interests',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                color: Color.fromARGB(251, 0, 0, 0))),
-                        SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.032),
-                      ],
+      body: AnimationLimiter(
+        child: SafeArea(
+            child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05,
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                child: ListView(
+                  children: [
+                    Center(
+                      child: Column(
+                        children: [
+                          const Text('Hello, Darius!',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromARGB(235, 237, 112, 23),
+                                fontSize: 16,
+                              )),
+                          const SizedBox(height: 8),
+                          const Text(
+                              'Please select 3 genres that you like to read. This will help us recommend books  based on your interests',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromARGB(251, 0, 0, 0))),
+                          SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.032),
+                        ],
+                      ),
                     ),
-                  ),
-                  grid(width, height),
-                  Button(
-                    text: 'Next',
-                    onTap: () => Get.to(const HomePage()),
-                  )
-                ],
-              ))),
+                    grid(width, height),
+                    Button(
+                      text: 'Next',
+                      onTap: () => Get.to(const HomePage()),
+                    )
+                  ],
+                ))),
+      ),
     );
   }
 }
