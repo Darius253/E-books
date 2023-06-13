4afb8972aa5ebcd4c198497a83d275b286fcc86a
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-13 00:42:57 +0000

    Made few changes

--- lib/screens/authenticate/on_boarding/data.dart
+++ lib/screens/authenticate/on_boarding/data.dart
@@ -1,27 +1,112 @@
+import 'package:flutter/material.dart';
 import '../../../shared/exports.dart';
 
-class OnBoardData {
-  String svgimg;
-  String title;
-  String desc;
-  OnBoardData({required this.svgimg, required this.title, required this.desc});
+Widget onboarding(
+  BuildContext context,
+  String title,
+  String subtitle,
+  String image,
+  Color color,
+  Color textColor,
+  Color nextColor,
+  Color titleColor,
+  Function()? onTap,
+  Function()? skip,
+) {
+  return Container(
+    color: color,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.6,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(32),
+          ),
+          child: Center(
+            child: SvgPicture.asset(
+              image,
+              height: MediaQuery.of(context).size.height * 0.5,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            color: titleColor,
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.02,
+        ),
+        Text(
+          subtitle,
+          style: TextStyle(
+            color: textColor,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.05,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: skip,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.085,
+                decoration: BoxDecoration(
+                  color: color,
+                  border: Border.all(color: textColor),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      color: textColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            InkWell(
+              onTap: onTap,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.4,
+                height: MediaQuery.of(context).size.height * 0.085,
+                decoration: BoxDecoration(
+                  color: textColor,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Text(
+                    'Next',
+                    style: TextStyle(
+                      color: nextColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
-
-List<OnBoardData> ondata = [
-  OnBoardData(
-      svgimg: Images.welcome,
-      title: 'Welcome to Awstore',
-      desc: "The ultimate destination for buying and selling digital arts and books."),
-  OnBoardData(
-      svgimg: Images.thing,
-      title: 'Find your thing',
-      desc: "The app offers a wide selection of digital arts and books, including paintings, photographs, ebooks,"),
-  OnBoardData(
-      svgimg: Images.interface,
-      title: 'Easy-to-use interface',
-      desc: "Awstore has an easy-to-use interface that allows users to search for specific items or browse by category."),
-      OnBoardData(
-      svgimg: Images.profit,
-      title: 'Profit from your work',
-      desc: "This is the perfect platform for artists and authors. Create a profile, upload your artwork, and sell to your work to buyers all over the world."),
-];
