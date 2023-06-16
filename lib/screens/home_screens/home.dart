84777f4b99ce61179bb8b80bcef79bc914110d3c
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-16 20:17:21 +0000

    Made changeds

--- lib/screens/home_screens/home.dart
+++ lib/screens/home_screens/home.dart
@@ -8,10 +8,14 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with AutomaticKeepAliveClientMixin<Home> {
   bool bookshelf = true;
   @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
@@ -30,101 +34,96 @@ class _HomeState extends State<Home> {
         ),
         SliverToBoxAdapter(
             child: Padding(
-              
-              padding:  EdgeInsets.only(bottom:height*0.05),
-              child: bookshelf ? const BookShelf() : const ArtGallery(),
-            )),
+          padding: EdgeInsets.only(bottom: height * 0.05),
+          child: bookshelf ? const BookShelf() : const ArtGallery(),
+        )),
       ],
     ));
   }
 
   //APPBar
   Widget appBar(double height, double width) {
-    return Padding(
-      padding: EdgeInsets.only(
-        right: width * 0,
-      ),
-      child: SizedBox(
-        height: height * 0.1,
-        child: Row(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
+    return SizedBox(
+      height: height * 0.1,
+      width: width,
+      child: Row(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () {
+                  print('Bookshelf');
+                  setState(() {
+                    bookshelf = true;
+                  });
+                },
+                child: Text(
+                  'Bookshelf',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: bookshelf
+                          ? const Color.fromARGB(255, 237, 112, 23)
+                          : Colors.grey),
+                ),
+              ),
+              Container(
+                  width: width * 0.1,
+                  height: height * 0.005,
+                  color: bookshelf
+                      ? const Color.fromARGB(255, 237, 112, 23)
+                      : Colors.transparent)
+            ],
+          ),
+          SizedBox(
+            width: width * 0.02,
+          ),
+          Container(
+            color: Colors.grey,
+            width: width * 0.003,
+            height: height * 0.05,
+          ),
+          SizedBox(
+            width: width * 0.02,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
                   onTap: () {
-                    print('Bookshelf');
+                    print('Art Gallery');
                     setState(() {
-                      bookshelf = true;
+                      bookshelf = false;
                     });
                   },
                   child: Text(
-                    'Bookshelf',
+                    'Art Gallery',
                     style: TextStyle(
                         fontWeight: FontWeight.w600,
                         fontSize: 16,
-                        color: bookshelf
+                        color: !bookshelf
                             ? const Color.fromARGB(255, 237, 112, 23)
                             : Colors.grey),
-                  ),
-                ),
-                Container(
-                    width: width * 0.15,
-                    height: height * 0.005,
-                    color: bookshelf
-                        ? const Color.fromARGB(255, 237, 112, 23)
-                        : Colors.transparent)
-              ],
-            ),
-            SizedBox(
-              width: width * 0.02,
-            ),
-            Container(
-              color: Colors.grey,
-              width: width * 0.003,
-              height: height * 0.05,
-            ),
-            SizedBox(
-              width: width * 0.02,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                InkWell(
-                    onTap: () {
-                      print('Art Gallery');
-                      setState(() {
-                        bookshelf = false;
-                      });
-                    },
-                    child: Text(
-                      'Art Gallery',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: !bookshelf
-                              ? const Color.fromARGB(255, 237, 112, 23)
-                              : Colors.grey),
-                    )),
-                Container(
-                    width: width * 0.15,
-                    height: height * 0.005,
-                    color: !bookshelf
-                        ? const Color.fromARGB(255, 237, 112, 23)
-                        : Colors.transparent)
-              ],
-            ),
-            const Expanded(child: SizedBox()),
-            IconButton(
-                onPressed: () {
-                  Get.to(const ProfilePage());
-                },
-                icon: const Icon(
-                  Icons.menu,
-                  color: Color.fromARGB(255, 41, 45, 50),
-                ))
-          ],
-        ),
+                  )),
+              Container(
+                  width: width * 0.1,
+                  height: height * 0.005,
+                  color: !bookshelf
+                      ? const Color.fromARGB(255, 237, 112, 23)
+                      : Colors.transparent)
+            ],
+          ),
+          const Expanded(child: SizedBox()),
+          IconButton(
+              onPressed: () {
+                Get.to(const ProfilePage());
+              },
+              icon: const Icon(
+                Icons.menu,
+                color: Color.fromARGB(255, 41, 45, 50),
+              ))
+        ],
       ),
     );
   }
