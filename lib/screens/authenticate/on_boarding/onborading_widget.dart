bf2389542479b72713bebcb52fc85eaa302ae534
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-16 20:18:23 +0000

    Made changes

--- lib/screens/authenticate/on_boarding/onborading_widget.dart
+++ lib/screens/authenticate/on_boarding/onborading_widget.dart
@@ -12,6 +12,7 @@ Widget onboarding(
   Color titleColor,
   Function()? onTap,
   Function()? skip,
+  bool? lastIndex,
 ) {
   return Container(
     color: color,
@@ -20,9 +21,11 @@ Widget onboarding(
       children: [
         Container(
           height: MediaQuery.of(context).size.height * 0.6,
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.circular(32),
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(32),
+                bottomRight: Radius.circular(32)),
           ),
           child: Center(
             child: SvgPicture.asset(
@@ -31,80 +34,91 @@ Widget onboarding(
             ),
           ),
         ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
-        ),
-        Text(
-          title,
-          style: TextStyle(
-            color: titleColor,
-            fontSize: 28,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.02,
-        ),
-        Text(
-          subtitle,
-          style: TextStyle(
-            color: textColor,
-          ),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.05,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            InkWell(
-              onTap: skip,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.4,
-                height: MediaQuery.of(context).size.height * 0.085,
-                decoration: BoxDecoration(
-                  color: color,
-                  border: Border.all(color: textColor),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Center(
-                  child: Text(
-                    'Skip',
-                    style: TextStyle(
-                      color: textColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
+        Padding(
+          padding:  EdgeInsets.symmetric(horizontal:MediaQuery.of(context).size.width *0.08),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  color: titleColor,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w600,
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            InkWell(
-              onTap: onTap,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.4,
-                height: MediaQuery.of(context).size.height * 0.085,
-                decoration: BoxDecoration(
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Text(
+                subtitle,
+                style: TextStyle(
                   color: textColor,
-                  borderRadius: BorderRadius.circular(12),
                 ),
-                child: Center(
-                  child: Text(
-                    'Next',
-                    style: TextStyle(
-                      color: nextColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
+                textAlign: TextAlign.center,
               ),
-            ),
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+              ),
+              lastIndex == true
+                  ? GetStartedButton(
+                      onPressed: () => Get.to(() => const SelectSignUp()))
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: skip,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            height: MediaQuery.of(context).size.height * 0.08,
+                            decoration: BoxDecoration(
+                              color: color,
+                              border: Border.all(color: textColor),
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Skip',
+                                style: TextStyle(
+                                  color: textColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        InkWell(
+                          onTap: onTap,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            height: MediaQuery.of(context).size.height * 0.08,
+                            decoration: BoxDecoration(
+                              color: textColor,
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Next',
+                                style: TextStyle(
+                                  color: nextColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+            ],
+          ),
         ),
       ],
     ),
