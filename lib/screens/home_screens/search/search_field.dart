ac09ecb9967c52088516089e8c0cc7828d00e911
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-27 15:49:29 +0000

    Made changes

--- lib/screens/home_screens/search/search_field.dart
+++ lib/screens/home_screens/search/search_field.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/cupertino.dart';
-import 'package:reader_app/shared/exports.dart';
+import 'package:flutter/material.dart';
+
+import 'package:reader_app/shared/constants.dart';
+
+import '../../../shared/exports.dart';
 
 class SearchField extends StatefulWidget {
   const SearchField({super.key});
@@ -9,32 +12,72 @@ class SearchField extends StatefulWidget {
 }
 
 class _SearchFieldState extends State<SearchField> {
+  final controller = TextEditingController();
+  String? search;
   @override
   Widget build(BuildContext context) {
-    return CupertinoNavigationBar(
-      backgroundColor: Palette.primary,
-      middle: Padding(
-        padding:
-            const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        child: CupertinoSearchTextField(
-          onSubmitted: (value) {
-            
-          },
-          prefixIcon: const Icon(
-            CupertinoIcons.search,
-            size: 16,
+    double width = MediaQuery.sizeOf(context).width;
+    double height = MediaQuery.sizeOf(context).height;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: width * 0.044),
+      color: Palette.primary,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: height * 0.044),
+        child: Container(
+          width: width * 0.8,
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
           ),
-          backgroundColor: Palette.white,
-          borderRadius: BorderRadius.circular(13),
-          style: const TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
-              color: Color.fromARGB(255, 71, 71, 71)),
-          placeholder: 'Title, author, or artist',
-          placeholderStyle: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w400,
-              color: Palette.grey),
+          child: Padding(
+            padding: EdgeInsets.only(
+              left: width * 0.016,
+            ),
+            child: Center(
+              child: ListTile(
+                horizontalTitleGap: 8,
+                minLeadingWidth: 8,
+                leading: Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: PhosphorIcon(
+                    PhosphorIcons.regular.magnifyingGlass,
+                    color: Colors.grey.withOpacity(0.6),
+                  ),
+                ),
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 18.0),
+                  child: TextFormField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Title, author or artist',
+                      hintStyle: TextStyle(
+                        color:  Colors.grey.withOpacity(0.6),
+                        fontSize: 14,
+                        fontFamily: 'Open Sans',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        search = value;
+                      });
+                    },
+                    onFieldSubmitted: (value) {
+                      setState(() {
+                        search = value;
+                      });
+                    },
+                  ),
+                ),
+                trailing: Padding(
+                  padding: const EdgeInsets.only(bottom: 15.0),
+                  child: PhosphorIcon(PhosphorIcons.regular.funnelSimple),
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
