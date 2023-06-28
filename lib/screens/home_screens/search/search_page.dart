3881b11d227ee6e00237668122716ba9def4e007
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-28 02:58:27 +0000

    Made changes

--- lib/screens/home_screens/search/search_page.dart
+++ lib/screens/home_screens/search/search_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:reader_app/screens/home_screens/search/filter_page.dart';
 import 'package:reader_app/shared/exports.dart';
 
-class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
+class SearchPage extends StatefulWidget {
+  const SearchPage({
+    super.key,
+  });
 
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final _controlller = TextEditingController();
+  String? search;
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
@@ -11,31 +21,47 @@ class SearchPage extends StatelessWidget {
       child: Scaffold(
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(height * 0.15),
-          child: const SearchField(),
+          child: SearchField(
+            controller: _controlller,
+            onSearch: (value) {
+              setState(() {
+                search = value;
+              });
+              print(search);
+            },
+          ),
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                const Text(
-                  'Browse Tags',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-                SizedBox(
-                  height: height * 0.02,
-                ),
-                SizedBox(
-                    height: height * 0.75, child: const SearchTags()),
-              ],
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
             ),
+            child: search != null
+                ? FilterPage(
+                    search: search,
+                  )
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      const Text(
+                        'Browse Tags',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      SizedBox(
+                        height: height * 0.75,
+                        child: const SearchTags(),
+                      ),
+                    ],
+                  ),
           ),
         ),
       ),
