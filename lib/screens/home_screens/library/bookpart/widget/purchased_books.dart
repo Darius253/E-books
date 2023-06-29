99042334f697bc7c5a358c948cb07d960b4c2a63
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-29 01:02:00 +0000

    Made changes

---
+++ lib/screens/home_screens/library/bookpart/widget/purchased_books.dart
@@ -0,0 +1,35 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:reader_app/shared/exports.dart';
+
+class PurchasedBooks extends StatelessWidget {
+  const PurchasedBooks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: MasonryGridView.builder(
+        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2),
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 5,
+        itemCount: book.length,
+        itemBuilder: (BuildContext context, int index) {
+          return books(
+              width,
+              Random().nextInt(150) + 50.5,
+              book[index].author,
+              book[index].title,
+              book[index].genre,
+              book[index].price,
+              14,
+              context);
+        },
+      ),
+    );
+  }
+}
