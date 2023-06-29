cbb5eda9ea74e2dc37b545d4ad418a99be80ebf8
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-29 01:02:40 +0000

    Made changes

---
+++ lib/screens/home_screens/library/artstorepart/artstore_library.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:reader_app/screens/home_screens/library/artstorepart/purchased_arts.dart';
+import 'package:reader_app/shared/exports.dart';
+
+class ArtStoreLibrary extends StatefulWidget {
+  const ArtStoreLibrary({super.key});
+
+  @override
+  State<ArtStoreLibrary> createState() => _ArtStoreLibraryState();
+}
+
+class _ArtStoreLibraryState extends State<ArtStoreLibrary> {
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    return Column(
+      children: [
+        purchaseButton('Purchases'),
+        const SizedBox(
+          height: 26,
+        ),
+        SizedBox(
+          height: height * 0.46,
+          child: const PurchasedArts(),
+        ),
+      ],
+    );
+  }
+}
