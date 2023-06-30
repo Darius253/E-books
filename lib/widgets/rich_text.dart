5375e21b3bd2405b03acc5a201b086dad610892d
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-30 01:58:19 +0000

    Made changes

--- lib/widgets/rich_text.dart
+++ lib/widgets/rich_text.dart
@@ -2,28 +2,41 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:reader_app/shared/exports.dart';
 
-class RichTextWidget extends StatelessWidget {
-  const RichTextWidget({
-    super.key,
-  });
+Widget signIn(BuildContext context) {
+  return RichText(
+    text: TextSpan(
+      style: DefaultTextStyle.of(context).style,
+      children: <TextSpan>[
+        const TextSpan(text: 'Already have an account? '),
+        TextSpan(
+          text: 'Login',
+          style: const TextStyle(color: Palette.primary),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              Get.to(() => const SignIn());
+            },
+        ),
+      ],
+    ),
+  );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-        style: DefaultTextStyle.of(context).style,
-        children: <TextSpan>[
-          const TextSpan(text: 'Already have an account? '),
-          TextSpan(
-            text: 'Login',
-            style: const TextStyle(color: Palette.primary),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                Get.to(() => const SignIn());
-              },
-          ),
-        ],
-      ),
-    );
-  }
+Widget signUp(BuildContext context) {
+  return RichText(
+    text: TextSpan(
+      children: <TextSpan>[
+        const TextSpan(
+            text: 'Are you need new here? ',
+            style: TextStyle(color: Colors.black)),
+        TextSpan(
+          text: 'Signup',
+          style: const TextStyle(color: Palette.primary),
+          recognizer: TapGestureRecognizer()
+            ..onTap = () {
+              Get.to(() => const SignUpPage());
+            },
+        ),
+      ],
+    ),
+  );
 }
