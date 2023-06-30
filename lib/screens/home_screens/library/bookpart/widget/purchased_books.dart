0e7128da0c748b75eeed212af38ad22af3dcc9d5
Darius <68332672+Darius253@users.noreply.github.com>
2023-06-30 02:04:05 +0000

    Made changes

--- lib/screens/home_screens/library/bookpart/widget/purchased_books.dart
+++ lib/screens/home_screens/library/bookpart/widget/purchased_books.dart
@@ -10,26 +10,23 @@ class PurchasedBooks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: MasonryGridView.builder(
-        gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2),
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 5,
-        itemCount: book.length,
-        itemBuilder: (BuildContext context, int index) {
-          return books(
-              width,
-              Random().nextInt(150) + 50.5,
-              book[index].author,
-              book[index].title,
-              book[index].genre,
-              book[index].price,
-              14,
-              context);
-        },
-      ),
+    return MasonryGridView.builder(
+      gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2),
+      mainAxisSpacing: 16,
+      crossAxisSpacing: 5,
+      itemCount: book.length,
+      itemBuilder: (BuildContext context, int index) {
+        return books(
+            width,
+            Random().nextInt(150) + 50.5,
+            book[index].author,
+            book[index].title,
+            book[index].genre,
+            book[index].price,
+            14,
+            context);
+      },
     );
   }
 }
