b6d796b0b80469041026f1ee06491cf2d105b120
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-16 00:47:28 +0000

    Login integration

---
+++ lib/services/api_constants.dart
@@ -0,0 +1,5 @@
+const String api = 'https://readerapp-production.herokuapp.com';
+const String loginPath = '/ebook/api/v1/login?';
+const String signupPath = '/ebook/api/v1/signup?';
+
+
---
+++ lib/services/login_api.dart
@@ -0,0 +1,55 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:reader_app/services/api_constants.dart';
+import 'package:reader_app/shared/exports.dart';
+
+class Login {
+  Future<void> login(String email, String password) async {
+    try {
+      final dio = Dio();
+
+      // Define the headers
+      final headers = {
+        'Access-Control-Allow-Origin': 'false',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-Content-Type-Options': 'nosniff',
+        'X-Download-Options': 'noopen',
+        'X-Frame-Options': 'DENY',
+        'X-XSS-Protection': '1; mode=block',
+      };
+
+      // Set the headers in the Dio instance
+      dio.options.headers = headers;
+
+      final response = await dio.post(
+        '$api$loginPath',
+        data: {'email': email, 'password': password},
+      );
+
+      // Handle the response here
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        print(response.data['jwt']['account_type']);
+         print(response.data['jwt']['token']);
+
+      
+      } else {
+        Get.snackbar(
+          'Login Failed:',
+          '${response.data['message']}',
+          duration: const Duration(seconds: 5),
+          colorText: Colors.red,
+          backgroundColor: Colors.black,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      }
+    } catch (error) {
+      print('Error during login: $error');
+    }
+  }
+}
+
+void main() {
+  final login = Login();
+  login.login('user@example.com', 'password');
+}
