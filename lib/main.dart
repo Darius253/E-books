dc72667e1a317b04b91c87bf7e4b11039396a920
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-21 12:18:08 +0000

    ✨✔Added hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:reader_app/models/boxes.dart';
+import 'package:reader_app/models/favGenres.dart';
+import 'package:reader_app/models/person.dart';
 import 'package:reader_app/shared/exports.dart';
 
-void main() => runApp(const App());
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(PersonAdapter());
+  Hive.registerAdapter(FavGenresAdapter());
+  boxPersons = await Hive.openBox<Person>('userDetails');
+  boxGenres = await Hive.openBox<FavGenres>('favGenres');
+
+  runApp(const App());
+}
