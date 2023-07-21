9fed48c29e554ee0d3e6875202c72c1309a30669
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-21 12:20:22 +0000

    ✨✔Added hive

--- lib/services/login_api.dart
+++ lib/services/login_api.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:reader_app/models/boxes.dart';
+import 'package:reader_app/models/person.dart';
+import 'package:reader_app/screens/authenticate/new_user/genre.dart';
 import 'package:reader_app/services/api_constants.dart';
 import 'package:reader_app/shared/exports.dart';
 
@@ -29,14 +32,18 @@ class Login {
 
       // Handle the response here
       if (response.statusCode == 200 && response.data['success'] == true) {
-        print(response.data['jwt']['account_type']);
-         print(response.data['jwt']['token']);
-
-      
+        boxPersons.put(
+          'personDetails',
+          Person(
+              name: response.data['jwt']['name'],
+              accountType: response.data['jwt']['account_type'],
+              userId: response.data['jwt']['user_id']),
+        );
+        Get.to(() => const SelectGenre());
       } else {
         Get.snackbar(
           'Login Failed:',
-          'Kindly check your details provided.',
+          'Kindly provide the correct credentials',
           duration: const Duration(seconds: 5),
           colorText: Colors.red,
           backgroundColor: Colors.black,
@@ -44,16 +51,15 @@ class Login {
         );
       }
     } catch (error) {
-       Get.snackbar(
-          'Login Failed:',
-          'Something happened. Please Try again later.',
-          duration: const Duration(seconds: 5),
-          colorText: Colors.red,
-          backgroundColor: Colors.black,
-          snackPosition: SnackPosition.BOTTOM,
-        );
+      print(error);
+      Get.snackbar(
+        'Login Failed:',
+        'Something happened. Please Try again later.',
+        duration: const Duration(seconds: 5),
+        colorText: Colors.red,
+        backgroundColor: Colors.black,
+        snackPosition: SnackPosition.BOTTOM,
+      );
     }
   }
 }
-
-
