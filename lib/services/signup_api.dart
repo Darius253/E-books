140ae2a2ff60662fab4438d79b9562b02e89d982
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-21 12:20:28 +0000

    ✨✔Added hive

--- lib/services/signup_api.dart
+++ lib/services/signup_api.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:reader_app/models/boxes.dart';
+import 'package:reader_app/models/person.dart';
 import 'package:reader_app/services/api_constants.dart';
 import 'package:reader_app/shared/exports.dart';
 
@@ -10,46 +12,50 @@ class SignUp {
     String email,
     String phoneNumber,
     String password,
-    int accountType,
   ) async {
+    final dio = Dio();
+
+    // Define the headers
+    final headers = {
+      'Access-Control-Allow-Origin': 'false',
+      'Connection': 'keep-alive',
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-Content-Type-Options': 'nosniff',
+      'X-Download-Options': 'noopen',
+      'X-Frame-Options': 'DENY',
+      'X-XSS-Protection': '1; mode=block',
+    };
+
+    // Set the headers in the Dio instance
+    dio.options.headers = headers;
+
+    final response = await dio.post(
+      '$api$signupPath',
+      data: {
+        'email': email,
+        'password': password,
+        'username': username,
+        'name': fullName,
+        'mobile_money_number': phoneNumber,
+        'account_type': 3,
+      },
+    );
+
+    // Handle the response here
     try {
-      final dio = Dio();
-
-      // Define the headers
-      final headers = {
-        'Access-Control-Allow-Origin': 'false',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json; charset=utf-8',
-        'X-Content-Type-Options': 'nosniff',
-        'X-Download-Options': 'noopen',
-        'X-Frame-Options': 'DENY',
-        'X-XSS-Protection': '1; mode=block',
-      };
-
-      // Set the headers in the Dio instance
-      dio.options.headers = headers;
-
-      final response = await dio.post(
-        '$api$signupPath',
-        data: {
-          'email': email,
-          'password': password,
-          'username': username,
-          'name': fullName,
-          'mobile_money_number': phoneNumber,
-          'account_type': accountType,
-        },
-      );
-
-      // Handle the response here
       if (response.statusCode == 200 && response.data['success'] == true) {
-        print(response.data['jwt']['account_type']);
-        print(response.data['jwt']['token']);
+        print(response.data);
+        boxPersons.put(
+          'personDetails',
+          Person(
+              name: response.data['jwt']['name'],
+              accountType: response.data['jwt']['account_type'],
+              userId: response.data['jwt']['user_id']),
+        );
       } else {
-        print(response.data['errors']['detail']);
         Get.snackbar(
           'Account Creation Failed:',
-          response.data['errors']['detail'],
+          response.data['errors'][0]['detail'],
           duration: const Duration(seconds: 5),
           colorText: Colors.red,
           backgroundColor: Colors.black,
@@ -57,10 +63,73 @@ class SignUp {
         );
       }
     } catch (error) {
-      print('Error Creating Account: $error');
       Get.snackbar(
         'Error Creating Account:',
-        error.toString(),
+        response.data['errors'][0]['detail'],
+        duration: const Duration(seconds: 5),
+        colorText: Colors.red,
+        backgroundColor: Colors.black,
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  Future<void> creatorSignup(
+    String username,
+    String fullName,
+    String email,
+    String phoneNumber,
+    String password,
+    int accountType,
+  ) async {
+    final dio = Dio();
+
+    // Define the headers
+    final headers = {
+      'Access-Control-Allow-Origin': 'false',
+      'Connection': 'keep-alive',
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-Content-Type-Options': 'nosniff',
+      'X-Download-Options': 'noopen',
+      'X-Frame-Options': 'DENY',
+      'X-XSS-Protection': '1; mode=block',
+    };
+
+    // Set the headers in the Dio instance
+    dio.options.headers = headers;
+
+    final response = await dio.post(
+      '$api$signupPath',
+      data: {
+        'email': email,
+        'password': password,
+        'username': username,
+        'name': fullName,
+        'mobile_money_number': phoneNumber,
+        'account_type': accountType,
+      },
+    );
+
+    // Handle the response here
+    try {
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        print(response.data);
+        // print(response.data['jwt']['account_type']);
+        // print(response.data['jwt']['token']);
+      } else {
+        Get.snackbar(
+          'Account Creation Failed:',
+          response.data['errors'][0]['detail'],
+          duration: const Duration(seconds: 5),
+          colorText: Colors.red,
+          backgroundColor: Colors.black,
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      }
+    } catch (error) {
+      Get.snackbar(
+        'Error Creating Account:',
+        response.data['errors'][0]['detail'],
         duration: const Duration(seconds: 5),
         colorText: Colors.red,
         backgroundColor: Colors.black,
