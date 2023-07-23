9a89db373b770d50315222ddae76d3aa0a5465af
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-23 00:20:07 +0000

    Backend integration

--- lib/services/login_api.dart
+++ lib/services/login_api.dart
@@ -8,36 +8,39 @@ import 'package:reader_app/shared/exports.dart';
 
 class Login {
   Future<void> login(String email, String password) async {
+    final dio = Dio();
+
+    // Define the headers
+    final headers = {
+      'Access-Control-Allow-Origin': 'false',
+      'Connection': 'keep-alive',
+      'Content-Type': 'application/json; charset=utf-8',
+      'X-Content-Type-Options': 'nosniff',
+      'X-Download-Options': 'noopen',
+      'X-Frame-Options': 'DENY',
+      'X-XSS-Protection': '1; mode=block',
+    };
+
+    // Set the headers in the Dio instance
+    dio.options.headers = headers;
+
+    final response = await dio.post(
+      '$api$loginPath',
+      data: {'email': email, 'password': password},
+    );
+
+    // Handle the response here
     try {
-      final dio = Dio();
-
-      // Define the headers
-      final headers = {
-        'Access-Control-Allow-Origin': 'false',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json; charset=utf-8',
-        'X-Content-Type-Options': 'nosniff',
-        'X-Download-Options': 'noopen',
-        'X-Frame-Options': 'DENY',
-        'X-XSS-Protection': '1; mode=block',
-      };
-
-      // Set the headers in the Dio instance
-      dio.options.headers = headers;
-
-      final response = await dio.post(
-        '$api$loginPath',
-        data: {'email': email, 'password': password},
-      );
-
-      // Handle the response here
       if (response.statusCode == 200 && response.data['success'] == true) {
         boxPersons.put(
           'personDetails',
           Person(
-              name: response.data['jwt']['name'],
-              accountType: response.data['jwt']['account_type'],
-              userId: response.data['jwt']['user_id']),
+            name: response.data['jwt']['name'],
+            accountType: response.data['jwt']['account_type'],
+            country: response.data['jwt']['country'],
+            userId: response.data['jwt']['user_id'],
+            token: response.data['jwt']['token'],
+          ),
         );
         Get.to(() => const SelectGenre());
       } else {
