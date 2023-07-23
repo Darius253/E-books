b561ff01deedd68d72709a89129ddf4c7a42b581
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-23 00:19:21 +0000

    Backend integration

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,5 +1,4 @@
 const String api = 'https://readerapp-production.herokuapp.com';
 const String loginPath = '/ebook/api/v1/login?';
 const String signupPath = '/ebook/api/v1/signup?';
-
-
+const String genrePath = '/ebook/api/v1/categories';
