471ee5d8a8185a18701b0033c74d8e6fafabdc17
Darius <68332672+Darius253@users.noreply.github.com>
2023-07-23 00:16:37 +0000

    Backend integration

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -4,7 +4,13 @@ part 'person.g.dart';
 
 @HiveType(typeId: 1)
 class Person {
-  Person({required this.name, required this.accountType, required this.userId});
+  Person({
+    required this.name,
+    required this.accountType,
+    required this.userId,
+    required this.token,
+    required this.country,
+  });
   @HiveField(0)
   String name;
 
@@ -13,4 +19,10 @@ class Person {
 
   @HiveField(2)
   String userId;
+
+  @HiveField(3)
+  String token;
+
+  @HiveField(4)
+  String country;
 }
